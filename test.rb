dca805b4f5bd1a6589f30162b88bcae92d7002fa
Panadda <49682794+PanaddaManarata@users.noreply.github.com>
2019-09-17 15:01:01 +0200

    update test.rb

--- test.rb
+++ test.rb
@@ -1,9 +1,14 @@
-File.open("text_list.txt").each do |line|
+File.open("test_list.txt").each do |line|
     puts line
 end 
-
 
 =begin 
 file.open will look inside the txt file 
 . each do |line| will iterate over each line and put it in a seperate variable  
 =end 
+
+File.open("test_list.txt", "a") do |line|
+    line.puts "\r" + "go with the flow"
+end 
+
+# "a" appends, do anything after the end of test_list.txt

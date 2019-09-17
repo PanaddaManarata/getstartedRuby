b71e056bd12736b45eba550f4569b3bee20e8856
Panadda <49682794+PanaddaManarata@users.noreply.github.com>
2019-09-17 14:44:39 +0200

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,3 +1,9 @@
 File.open("text_list.txt").each do |line|
     puts line
 end 
+
+
+=begin 
+file.open will look inside the txt file 
+. each do |line| will iterate over each line and put it in a seperate variable  
+=end 
